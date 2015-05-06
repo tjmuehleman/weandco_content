f5299ae54f2b04d4fb4464cce238f796609805ad
Scott Murphy <scmurphy2009@gmail.com>
2015-05-06 11:42:10 -0400

    setup paperclip to use S3 for storage

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,5 +1,17 @@
 class ContentBlock < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_many :tools, foreign_key: "content_block_id"
+
+  has_attached_file :avatar,
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :use_timestamp => false,
+    :storage => :s3,
+    :s3_credentials  => {      
+      :bucket => ENV["BUCKET_NAME"],
+      :ACCESS_KEY_ID => ENV["AWS_ACCESS_KEY_ID"],
+      :SECRET_ACCESS_KEY => ENV["AWS_SECRET_ACCESS_KEY"]
+    },
+    :path => ":id/:style/:style_:basename.:extension",
+    :convert_options => { :all => '-background white -flatten +matte' }
+
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
